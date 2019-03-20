7deba1fdf1582cf802871768b6e077f8d13ed4ae
statisticallyfit <statisticallyfit@github.com>
2019-03-20 02:41:37 -0400

    working on assignment 1, question 2

--- STAT330 Statistical Learning/ASSIGNMENTS/Assignment1.R
+++ STAT330 Statistical Learning/ASSIGNMENTS/Assignment1.R
@@ -2,6 +2,7 @@ setwd("/datascience/projects/statisticallyfit/github/learningmathstat/RStatistic
 options(show.signif.stars = FALSE)
 
 library(ggplot2)
+library(ggfortify)
 
 bikeData <- read.csv("bike.csv", header=TRUE)
 
@@ -35,4 +36,30 @@ pairs(bikeData[,1:8]) # include all predictors but the last, since Casual = resp
 # series of dots, again indicating no linear correlation. 
 
 
+# part d) ------------------------------------------------------------------------
+bikeData$Weather <- factor(bikeData$Weather)
+bikeData$Year <- factor(bikeData$Year)
+
+
 # part c) --------------------------------------------------
+ggplot(data=bikeData, aes(x=Weather, y=Casual, group=Weather, colour=Weather)) + 
+      geom_boxplot(size=1) 
+
+# There are more casual bike users for mild weather (1 = clear, few clouds) than there
+# are for heavy thunderstorms (3) and mist (2), indicated by the greather height in
+# the boxplot at 1 than at 2 and 3. Also there is greater variation in casual bike users
+# for mild weather (1), signified by the larger IQR at 1 than for the other weathers. 
+
+# part e) ----------------------------------------------------------
+bike.lm <- lm(Casual ~ .- Date, data=bikeData)
+summary(bike.lm)
+
+autoplot(bike.lm)
+shapiro.test(bike.lm$residuals)
+# funnel shape in residuals, curvature - missing predictor?
+# residuals not normal
+# many outliers and leverage points
+
+# significant predictors: SeasonSpring, Year2012, Weekend, Temperature, Humidity, Windspeed
+# SeasonSpring - significantly more casual users in spring than autumn (base level)
+# Year2012 = significantly more casual users in 2012 than 2011. 
